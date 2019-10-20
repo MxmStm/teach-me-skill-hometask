c615bbf21eba0032d4a9cb363453e44522390920
Max <maksstoma@gmail.com>
2019-10-20 13:31:12 +0300

    add hometask1.3/add Task1_1

---
+++ src/main/java/com/teachmeskill/homework/hometask1_3/Task1_1.java
@@ -0,0 +1,12 @@
+package main.java.com.teachmeskill.homework.hometask1_3;
+
+public class Task1_1 {
+    public static void main (String [] args) {
+        int value1,value2;
+
+        value1=80;
+        value2=55;
+
+        System.out.println("Наибольшее из заданных чисел = "+ Math.max(value1,value2));
+    }
+}
