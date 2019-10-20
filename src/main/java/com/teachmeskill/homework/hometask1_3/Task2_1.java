c35e77509a31da8ab7347590748f491368785b4c
Max <maksstoma@gmail.com>
2019-10-20 14:39:49 +0300

    add hometask1_3

--- src/main/java/com/teachmeskill/homework/hometask1_3/Task2_1.java
+++ src/main/java/com/teachmeskill/homework/hometask1_3/Task2_1.java
@@ -1,12 +1,12 @@
 package main.java.com.teachmeskill.homework.hometask1_3;
 
-public class Task1_1 {
+public class Task2_1 {
     public static void main(String[] args) {
         int value1, value2;
 
-        value1 = 80;
+        value1 = 8;
         value2 = 55;
 
-        System.out.println("Наибольшее из заданных чисел = " + Math.max(value1, value2));
+        System.out.println("Наименьшее из заданных чисел = " + Math.min(value1, value2));
     }
 }
---
+++ src/main/java/com/teachmeskill/homework/hometask1_3/Task3.java
@@ -0,0 +1,17 @@
+package main.java.com.teachmeskill.homework.hometask1_3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int num1, num2, num3, num4;
+
+        num1 = -1;
+        num2 = -19;
+        num3 = 20;
+        num4 = 0;
+
+        System.out.println("Абсолютное значение числа " + num1 + " = " + Math.abs(num1));
+        System.out.println("Абсолютное значение числа " + num2 + " = " + Math.abs(num2));
+        System.out.println("Абсолютное значение числа " + num3 + " = " + Math.abs(num3));
+        System.out.println("Абсолютное значение числа " + num4 + " = " + Math.abs(num4));
+    }
+}
