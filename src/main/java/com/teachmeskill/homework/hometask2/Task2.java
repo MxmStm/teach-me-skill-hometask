cfa4cf3c29d929ef4084842cd53469477b98e47d
Max <maksstoma@gmail.com>
2019-10-20 22:16:26 +0300

    add hometask2

---
+++ src/main/java/com/teachmeskill/homework/hometask2/Task2.java
@@ -0,0 +1,10 @@
+package main.java.com.teachmeskill.homework.hometask2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int x = 10; x <= 20; x++) {
+            System.out.println(x + "^2 = " + Math.pow(x, 2));
+        }
+    }
+}
---
+++ src/main/java/com/teachmeskill/homework/hometask2/Task5.java
@@ -0,0 +1,13 @@
+package main.java.com.teachmeskill.homework.hometask2;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 7; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
