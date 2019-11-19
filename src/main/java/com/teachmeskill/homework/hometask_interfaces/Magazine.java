e47f6d57cdb952072fbc54166e9985241424112a
Max <maksstoma@gmail.com>
2019-11-20 00:00:58 +0300

    corrections

--- src/main/java/com/teachmeskill/homework/hometask_interfaces/Magazine.java
+++ src/main/java/com/teachmeskill/homework/hometask_interfaces/Magazine.java
@@ -1,11 +1,44 @@
 package main.java.com.teachmeskill.homework.hometask_interfaces;
 
+import java.util.Objects;
+
 public class Magazine implements Printable {
 
-    String name;
+    private String name;
+
+    public Magazine() {
+    }
 
     public Magazine(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Magazine magazine = (Magazine) o;
+        return Objects.equals(name, magazine.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine{" +
+                "name='" + name + '\'' +
+                '}';
     }
 
     @Override
--- src/main/java/com/teachmeskill/homework/hometask_interfaces_additional/Drum.java
+++ src/main/java/com/teachmeskill/homework/hometask_interfaces_additional/Drum.java
@@ -1,13 +1,56 @@
 package main.java.com.teachmeskill.homework.hometask_interfaces_additional;
 
+import java.util.Objects;
+
 public class Drum implements MusicInstrument {
 
-    double diameter;
-    double depth;
+    private double diameter;
+    private double depth;
+
+    public Drum() {
+    }
 
     public Drum(double diameter, double depth) {
         this.diameter = diameter;
         this.depth = depth;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Drum drum = (Drum) o;
+        return Double.compare(drum.diameter, diameter) == 0 &&
+                Double.compare(drum.depth, depth) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(diameter, depth);
+    }
+
+    @Override
+    public String toString() {
+        return "Drum{" +
+                "diameter=" + diameter +
+                ", depth=" + depth +
+                '}';
     }
 
     @Override
