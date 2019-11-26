583e00cece2e714f055537d1945d1370bed0297f
Max <maksstoma@gmail.com>
2019-11-26 19:17:37 +0300

    add hometask_collection

---
+++ src/main/java/com/teachmeskill/homework/hometask_collection/ProductSortPrice.java
@@ -0,0 +1,17 @@
+package main.java.com.teachmeskill.homework.hometask_collection;
+
+import java.util.Comparator;
+
+public class ProductSortPrice implements Comparator<Product> {
+
+    @Override
+    public int compare(Product o1, Product o2) {
+        if (o1.getPrice() > o2.getPrice()) {
+            return 1;
+        } else if (o1.getPrice() < o2.getPrice()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
